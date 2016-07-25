4b8680827216ab67336c2101bb9e822a0dea394b
Guigo2k <guigo2k@guigo2k.com>
2016-07-25 12:35:53 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var monitors = [
-    "m778001640-9d96fefa5673ce667011f1d7",  //Place your first Key Here
-    "m778001638-39ea236c63ae57a29a0cb520", //Place your next key here! <- Keep duplicating this line for more than 2 keys!
-    "m778001636-5dc662f4d630ec8199ed1484", //Place your next key here! <- Keep duplicating this line for more than 2 keys!
+    "m778001640-9d96fefa5673ce667011f1d7", // API development
+    "m778001638-39ea236c63ae57a29a0cb520", // API staging
+    "m778001636-5dc662f4d630ec8199ed1484", // API production
 
 ];
-var title = 'My Project Status Page'; //Page <title> and heading at top of page
-var refreshtime = 60; //How often to refresh in seconds
-var footermessage = ''; //HTML to go in footer
+var title = 'Budbuds.us Status'; // Page <title> and heading at top of page
+var refreshtime = 60; // How often to refresh in seconds
+var footermessage = ''; // HTML to go in footer
